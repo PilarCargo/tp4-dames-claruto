d34c72922ba3542cc90278984a40c35646ab66c1
Augusto Arrojo <augusto.arrojo@eleves.ec-nantes.fr>
2024-12-05 10:00:38 +0100

    Constructeurs

--- Jeu_dames/src/main/java/com/mycompany/jeu_dames/Joueur.java
+++ Jeu_dames/src/main/java/com/mycompany/jeu_dames/Joueur.java
@@ -1,40 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.jeu_dames;
 
-import java.util.ArrayList;
-
 /**
- *
- * @author Clara
+ * Represents a player in the game. A player has a name, a color, and is
+ * associated with a game board.
  */
-public class Joueur {
+public final class Joueur {
+
     private String nom;
-    private boolean couleur;
-    private TableauDeJeu partie;
-    
+    private final boolean couleur;
+    private final TableauDeJeu partie;
+
+    // 
+    /**
+     * Constructor to load a game
+     * <p>
+     * Constructs a player with a specified name, color, and associated game
+     * board. </p>
+     *
+     * @param nom The name of the player.
+     * @param couleur The color of the player (true for black or false for
+     * white).
+     * @param partie The game board associated with the player.
+     */
     public Joueur(String nom, boolean couleur, TableauDeJeu partie) {
         this.nom = nom;
         this.couleur = couleur;
         this.partie = partie;
     }
-    
-    public void tourDeJeu() {
-        
-    }
 
-    public String getNom() {
-        return nom;
+    /**
+     * Constructs a player for a new game, where the player’s name is
+     * initialized by the player during the game setup.
+     *
+     * @param couleur The color of the player (true for black or false for
+     * white).
+     * @param partie The game board associated with the player.
+     * @param numeroJoueur The number of the player, used to prompt for the
+     * name.
+     */
+    public Joueur(boolean couleur, TableauDeJeu partie, int numeroJoueur) {
+        this.couleur = couleur;
+        this.partie = partie;
+        //this.initializeNom(numeroJoueur);
     }
-
-    public boolean isCouleur() {
-        return couleur;
-    }
-
-    public TableauDeJeu getPartie() {
-        return partie;
-    }
-    
 }
